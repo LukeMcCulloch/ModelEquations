57d892c1653a7c35d2a608b9daca9e776f63e9c5
Luke McCulloch <Luke.McCulloch@bentley.com>
2019-05-07 14:56:39 -0500

    clarify results plotting

--- Burgers/newton/run/plot.py
+++ Burgers/newton/run/plot.py
@@ -4,16 +4,24 @@ import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 
 
-
+sclices = {1:[0,62],
+           2:[62,124],
+           3:[124,186],
+           4:[186,248],
+           5:[248,310],
+           6:[310,372]}
 
 xq1 = (np.loadtxt('EulerImplicit_FV.dat'))
 x1   = xq1[:,0]
 q1  = xq1[:,1]
 
 
+for sl in sclices:
+    rg = sclices[sl]
+    plt.plot(x1[rg[0]:rg[1]],
+             q1[rg[0]:rg[1]],label='Numeric U')
+    plt.axis(xmin=-5., ymin= -5., xmax=20.0, ymax=5.)
 
-plt.plot(x1,q1,label='Numeric U')
-plt.axis(xmin=-5., ymin= -5., xmax=20.0, ymax=5.)
 plt.grid()
 
 
